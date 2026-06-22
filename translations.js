c720af2bc21398aae60e92bf4875fddc91a8f2f4
Henry Brandt <henry_brandt@hotmail.com>
2026-06-22 11:50:31 +0200

    la in översättning på testamonial

--- translations.js
+++ translations.js
@@ -29,7 +29,7 @@ const translations = {
     "skill-react": "Vad jag kan om React.",
     "skill-azure": "Vad jag kan om Azure.",
     //testamonials
-    "testamonials-text": "riktigt cool kille -Hanna",
+    "testamonials-text": '"Riktigt cool kille" -Hanna',
     //project
     "project-tetris":
       "Skapade och designade en tetris sida med neon tema. Ett av mina absolut första projekt i HTML, CSS och JS.",
@@ -75,7 +75,7 @@ const translations = {
     "skill-react": "What I know about React.",
     "skill-azure": "What I know about Azure.",
     //testamonials
-    "testamonials-text": "real fine lad -Hanna",
+    "testamonials-text": '"Real fine lad" -Hanna',
     //project
     "project-tetris":
       "Created and designed a Tetris site with a neon theme. One of my very first projects in HTML, CSS and JS.",
